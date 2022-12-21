867483935df4cd74571d796aa198c1b090700d41
Juan Pablo Correa Tarazona <89850618+JuanPCT@users.noreply.github.com>
2022-12-21 13:17:58 -0500

    Categoria y productos

--- src/components/restaurante/Categoria.js
+++ src/components/restaurante/Categoria.js
@@ -1,19 +1,23 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import { IMAGES_ENDPOINT } from "../../helpers/endpoints";
 
 const Categoria = ({ cat }) => {
-  return (
-    <div className="card w-100 h-100" style={{ width: "18rem" }}>
-      <img
-        src={IMAGES_ENDPOINT + cat.imagen}
-        className="card-img-top h-50"
-        alt="..."
-      />
-      <div className="card-body">
-        <h6 className="card-title">{cat.nombre}</h6>
+    return (
+      <div className="col-6 col-md-3">
+          <Card className="card w-100 h-100">
+            <img
+              src={IMAGES_ENDPOINT + cat.imagen}
+              className="card-img-top img-fluid m-auto"
+              style={{width: "200px", height:"150px"}}
+              alt="..."
+            />
+            <div className="card-body">
+              <h6 className="card-title ">{cat.nombre}</h6>
+            </div>
+          </Card>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default Categoria;
